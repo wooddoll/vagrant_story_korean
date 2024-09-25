fdc399ebaecd377db5072571db7a39db13e6f14c
wooddoll <fly4moon@hotmail.com>
2024-09-25 17:39:46 +0900

    dialog scripts

---
+++ dialog/dialog.py
@@ -0,0 +1,384 @@
+from typing import Dict, Union, List
+from scripts import MPDstruct
+from pathlib import Path
+import pandas as pd
+import os
+from copy import deepcopy
+import logging
+import yaml
+from tqdm import tqdm
+
+def makeTBL():
+    pathFontsTxt = "D:/Projects/vagrant_story_korean/font/font14_table.txt"
+
+    lines = []
+    with open(pathFontsTxt, 'rt', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    tbl = {}
+
+    pos = 0
+    for r, line in enumerate(lines):        
+        for c, letter in enumerate(line):
+            if c >= 18: break
+
+            index = 18*r+c
+            if index >= 0xE5:
+                pos = index + 0xEC03
+            else:
+                pos = index
+
+            key = f"{pos:02X}" if pos <= 0xFF else f"{pos:04X}"
+            if letter == '\n':  letter = '↵'
+            if letter == '_':  letter = ' '
+            if letter == '□':  letter = ' '
+            tbl[key] = letter
+
+    
+    out_path = "D:/Projects/vagrant_story_korean/font/jpn.tbl"
+    with open(out_path, 'wt', encoding='utf-8') as file:
+        for k, v in tbl.items():
+            file.write(f"{str(k)}={str(v)}\n")
+
+def readTBL(path: str) -> Dict[int, str]:
+    lines = []
+    with open(path, 'rt', encoding='utf-8') as file:
+        lines = file.readlines()
+    
+    tbl = {}
+    for line in lines:
+        pos = line.find('=')
+        if pos == -1: continue
+        txts = line.split('=')
+        
+        tbl[int(txts[0], 16)] = txts[1][:-1]
+    
+    return tbl
+
+class convert_by_TBL():
+    def __init__(self, table: Union[str, dict]) -> None:
+        if isinstance(table, str):
+            self.fwd_tbl = readTBL(table)
+        elif isinstance(table, dict):
+            self.fwd_tbl = table
+        else:
+            logging.critical('table is not path or dict')
+
+        self.inv_tbl = {}
+        for k, v in self.fwd_tbl.items():
+            if len(v) != 1:
+                continue
+            
+            if k < 0xE5:
+                self.inv_tbl[v] = [k]
+            else:
+                v1 = (k >> 8) & 0xFF
+                v2 = k & 0xFF
+                self.inv_tbl[v] = [v1, v2]
+
+    def cvtBytes_str(self, bytesText: bytes) -> str:
+        pos = 0
+        length = len(bytesText)
+
+        strText = ''
+        while(pos < length):
+            letter = None
+            tmp = bytesText[pos]
+            pos += 1
+            
+            if tmp == 0xE8:
+                strText += '↵'
+                continue
+            elif tmp == 0xE7:
+                break
+            elif tmp > 0xE5:
+                tmp = (tmp << 8) | bytesText[pos]
+                pos += 1
+                letter = self.fwd_tbl.get(tmp)
+            else:
+                letter = self.fwd_tbl.get(tmp)
+
+            if letter is None:
+                strText += f'[{tmp:04X}]'
+            else:
+                strText += letter
+
+        return strText
+
+    def cvtStr_Bytes(self, strText: str) -> bytearray:
+        length = len(strText)
+        pos = 0
+        byteText = bytearray()
+        while pos < length:
+            tmp = ''
+            letter = strText[pos]
+            pos += 1
+            if letter == '[':
+                tmp = ''
+                while letter != ']':
+                    letter = strText[pos]
+                    pos += 1
+                    if letter == ']': break
+                    tmp += letter
+                byteText.append(int(tmp[:2], 16))
+                byteText.append(int(tmp[2:], 16))
+                continue
+            elif letter == '↵':
+                byteText.append(0xe7)
+                continue
+            else:
+                ret = self.inv_tbl.get(letter)
+                if ret is not None:
+                    byteText.extend(ret)
+
+        return byteText
+
+def checkSize(text: str):
+    rows = 1
+    cols = 1
+    _cols = 0
+
+    length = len(text)
+    pos = 0
+    while pos < length:
+        letter = text[pos]
+        pos += 1
+        if letter == '[':
+            while letter != ']':
+                letter = text[pos]
+                pos += 1
+            continue
+        elif letter == '↵':
+            rows += 1
+            _cols = 0
+        else:
+            _cols += 1
+            cols = max(cols, _cols)
+    return rows, cols
+
+def vertical2flat(text: str):
+    length = len(text)
+    pos = 0
+    flatText = ''
+    while pos < length:
+        tmp = ''
+        letter = text[pos]
+        pos += 1
+        if letter == '[':
+            tmp = '['
+            while letter != ']':
+                letter = text[pos]
+                tmp += letter
+                pos += 1
+            flatText += tmp
+            continue
+        elif letter == '↵':
+            continue
+        else:
+            flatText += letter
+
+    return flatText
+
+def flat2vertical(text: str):
+    length = len(text)
+    pos = 0
+    verticalText = ''
+    while pos < length:
+        tmp = ''
+        letter = text[pos]
+        pos += 1
+        if letter == '[':
+            tmp = '['
+            while letter != ']':
+                letter = text[pos]
+                tmp += letter
+                pos += 1
+            verticalText += tmp
+            continue
+        elif letter == '↵':
+            verticalText += '↵'
+        else:
+            verticalText += letter
+            verticalText += '↵'
+
+    if verticalText[-1] == '↵':
+        verticalText = verticalText[:-1]
+    return verticalText
+
+def readDictTable(path: str) -> List[List[str]]:
+    lines = []
+    with open(path, 'rt', encoding='utf-8') as file:
+        lines = file.readlines()
+    
+    table = []
+    for line in lines:
+        pos = line.find('=')
+        if pos == -1: continue
+        txts = line.split('=')
+        
+        table.append([txts[0], txts[1][:-1]])
+    
+    return table
+
+class ReplaceKeyword():
+    def __init__(self, table: Union[str, list]) -> None:
+        if isinstance(table, str):
+            self.table = readDictTable(table)
+        elif isinstance(table, list):
+            self.table = table
+        else:
+            logging.critical('table is not path or dict')
+    
+    def expandBytes(self, leftTable: convert_by_TBL, rightTable: convert_by_TBL):
+        for idx in range(len(self.table)):
+            left = self.table[idx][0]
+            self.table[idx].append(leftTable.cvtStr_Bytes(left))
+            right = self.table[idx][1]
+            self.table[idx].append(rightTable.cvtStr_Bytes(right))
+    
+    def replace(self, text:str):
+        outText = deepcopy(text)
+        for items in self.table:
+            outText = outText.replace(items[0], items[1])
+        return outText
+
+
+
+class Find_Word():
+    def __init__(self) -> None:
+        dictTable = ReplaceKeyword("D:/Projects/vagrant_story_korean/VSDictTable.tbl")
+        self.jpbtbl = convert_by_TBL("D:/Projects/vagrant_story_korean/font/jpn.tbl")
+        self.usatbl = convert_by_TBL("D:/Projects/vagrant_story_korean/font/usa.tbl")
+        dictTable.expandBytes(self.jpbtbl, self.usatbl)
+        self.dictTable = dictTable.table
+
+    def findWord_in_Bytes(self, data: bytes, word: bytes):
+        search_len = len(word)
+        results = []
+        for i in range(len(data) - search_len + 1):
+            if data[i:i + search_len] == word:
+                results.append(i)
+    
+        return results
+    
+    def find_Word_in_File(self, path: str):
+        fileBuffer = None
+        with open(path, 'rb') as file:
+            fileBuffer = file.read()
+
+        wordinfile = []
+        for item in self.dictTable:
+            poses = self.findWord_in_Bytes(fileBuffer, item[2])
+
+            if poses:
+                wordinfile.append([item[1], poses])
+        
+        return wordinfile
+    
+    def find_in_folder(self, input_path: str, outPath: str):
+        folder_path = Path(input_path)
+        file_list = [file for file in folder_path.rglob('*.MPD') if file.is_file()]
+
+        wordinfiles = []
+        for filepath in tqdm(file_list, desc="Processing"):
+            relative_path = filepath.relative_to(folder_path)
+            detected = self.find_Word_in_File(str(filepath))
+
+            if detected:
+                wordinfiles.append([str(relative_path), detected])
+
+        with open(outPath, 'w') as file:
+            yaml.dump(wordinfiles, file)
+
+findword = Find_Word()
+findword.find_in_folder("D:/Projects/vagrant_story_korean/", "find_in_folder.yaml")
+exit()
+
+def makeMPDtexts():
+    folder_path = Path('D:/Projects/vagrant_story_korean/font/test/jpn')
+    extension = '*.MPD'
+    file_list = list(folder_path.glob(extension))
+
+    dialogLists = []
+    jpnTBL = convert_by_TBL("D:/Projects/vagrant_story_korean/font/jpn.tbl")
+    keyTBL = ReplaceKeyword("VSDictTable.tbl")
+
+    for filepath in tqdm(file_list, desc="Processing"):
+        mpd = MPDstruct(str(filepath))
+
+        for idx, dialogBytes in enumerate(mpd.scriptSection.dialogText.dialogBytes):
+            text = jpnTBL.cvtBytes_str(dialogBytes)
+            rows, cols = checkSize(text)
+
+            singleRow = []
+            singleRow.append(filepath.stem)
+            singleRow.append(idx)            
+            singleRow.append(rows)
+            singleRow.append(cols)
+
+            if cols == 1:
+                text = vertical2flat(text)
+
+            singleRow.append(text)
+            # TODO
+            knText = keyTBL.replace(text)
+            singleRow.append(knText)
+            #singleRow.append(text)
+            dialogLists.append(singleRow)
+
+    df = pd.DataFrame(dialogLists, columns=['File', 'Index', 'rows', 'cols', 'Original', 'Translated'])
+    df.to_csv('VSdialog.csv', index=False)
+
+#makeMPDtexts()
+
+def readExelDialog(csv_path:str):
+    dialogLists = {}
+    df = pd.read_csv(csv_path)
+    num_rows, num_columns = df.shape
+    for idx in range(num_rows):
+        File = df.iloc[idx]['File']
+        Index = int(df.iloc[idx]['Index'])
+        rows = int(df.iloc[idx]['rows'])
+        cols = int(df.iloc[idx]['cols'])
+        original = df.iloc[idx]['Original']
+        translated = df.iloc[idx]['Translated']
+        if cols == 1:
+            translated = flat2vertical(translated)
+        
+        if dialogLists.get(File) is None:
+            dialogLists[File] = []
+        
+        prevSize = len(dialogLists[File])
+        if prevSize < (Index + 1):
+            newlist = [''] * (Index + 1)
+            for i in range(prevSize):
+                newlist[i] = dialogLists[File][i]
+            dialogLists[File] = newlist
+        dialogLists[File][Index] = translated
+
+    return dialogLists
+
+
+def importDialog2MPD(dialogLists):
+    original_folder_path = Path('D:/Projects/vagrant_story_korean/font/test/jpn')
+    output_folder_path = 'D:/Projects/vagrant_story_korean/'
+    # TODO replace to krTBL
+    jpnTBL = convert_by_TBL("D:/Projects/vagrant_story_korean/font/jpn.tbl")
+
+    extension = '*.MPD'
+    file_list = list(original_folder_path.glob(extension))
+    for filepath in tqdm(file_list, desc="Processing"):
+        mpd = MPDstruct(str(filepath))
+
+        filename = filepath.stem
+        texts = dialogLists.get(filename)
+        if texts is not None:
+            for idx, dialogBytes in enumerate(mpd.scriptSection.dialogText.dialogBytes):
+                byteText = jpnTBL.cvtStr_Bytes(texts[idx])
+                mpd.scriptSection.dialogText.dialogBytes[idx] = byteText
+
+        outpath = os.path.join(output_folder_path, f"{filename}.MPD")
+        mpd.packData(outpath)
+
+dialogLists = readExelDialog('VSdialog.csv')
+importDialog2MPD(dialogLists)
---
+++ dialog/scripts.py
@@ -0,0 +1,294 @@
+from typing import Union
+import io
+import os
+import logging
+import math
+
+def int4(buffer: bytes):
+    return int.from_bytes(buffer[0:4], byteorder='little')
+def int2(buffer: bytes):
+    return int.from_bytes(buffer[0:2], byteorder='little')
+
+def bytes4(num: int):
+    return num.to_bytes(4, byteorder='little', signed=False)
+def bytes2(num: int):
+    return num.to_bytes(2, byteorder='little', signed=False)
+def bytes1(num: int):
+    return num.to_bytes(1, byteorder='little', signed=False)
+
+def readHeader(byte_stream: io.BytesIO, num: int, byteSize: int):
+    headerValues = []
+    if byteSize == 1:
+        for _ in range(num):
+            headerValues.append(int(byte_stream.read(1)))
+    elif byteSize == 2:
+        for _ in range(num):
+            headerValues.append(int2(byte_stream.read(2)))
+    elif byteSize == 4:
+        for _ in range(num):
+            headerValues.append(int4(byte_stream.read(4)))
+
+    return headerValues
+
+
+class SectionBase():
+    def __init__(self, buffer: Union[bytes, None] = None) -> None:
+        self.buffer = None
+
+        if buffer is not None:
+            self.unpackData(buffer)
+
+    def __len__(self):
+        return len(self.buffer) if self.buffer is not None else 0
+    
+    def unpackData(self, buffer: bytes):
+        self.buffer = bytearray(buffer) if buffer is not None else None
+
+    def packData(self):
+        return self.buffer
+
+class DialogText(SectionBase):
+    def __init__(self, buffer: Union[bytes, None] = None) -> None:
+        self.buffer = None
+        self.dialogBytes = []
+
+        if buffer is not None:
+            self.unpackData(buffer)
+
+    def readHeader(self):
+        if self.buffer is None:
+            return [], []
+        
+        byte_stream = io.BytesIO(self.buffer)
+        maxSize = len(self.buffer)
+
+        numDialogs = int2(byte_stream.read(2))
+        ptrText = [2*numDialogs]
+        for i in range(numDialogs - 1):
+            ptrText.append(2*int2(byte_stream.read(2)))
+
+        lenText = []
+        for i in range(numDialogs - 1):
+            lenText.append(ptrText[i+1] - ptrText[i])
+        lenText.append(maxSize - ptrText[-1])
+
+        return ptrText, lenText
+    
+    def checkDialog(self):
+        for idx, text in enumerate(self.dialogBytes):
+            endpos = -1
+            for i, c in enumerate(text):
+                if c == 0xE7:
+                    endpos = i + 1
+                    break
+            if endpos == -1:
+                text.extend(bytes1(0xE7))
+                endpos = len(text)
+            text = text[:endpos]
+            size = len(text)
+            if size%2 == 1:
+                text.extend(bytes1(0xEB))
+
+            self.dialogBytes[idx] = text
+
+    def unpackData(self, buffer: bytes):
+        if buffer is not None:
+            self.buffer = bytearray(buffer)
+        else:
+            return
+        
+        ptrText, lenText = self.readHeader()
+
+        byte_stream = io.BytesIO(buffer)
+
+        self.dialogBytes = []
+        for pos, size in zip(ptrText, lenText):
+            if size == 0:
+                continue
+
+            byte_stream.seek(pos)
+
+            text = bytearray(byte_stream.read(1))
+            while(text[-1] != 0xE7):
+                text.extend(byte_stream.read(1))
+
+            if len(text) > size:
+                logging.critical(f"check the text end mark(E7), size overflowed({len(text)} > {size})")
+            
+            self.dialogBytes.append(text)
+
+    def packData(self):
+        if self.buffer is None:
+            return None
+        
+        self.checkDialog()
+
+        ptrText, lenText = self.readHeader()
+        preSize = len(self.buffer) - ptrText[0]
+
+        numTexts = len(self.dialogBytes)
+        if ptrText[0]//2 != numTexts:
+            logging.critical(f"check the number of dialogs, size different; privious({ptrText[0]//2}) != current({numTexts})")
+
+        ptrText.append(-1)
+        for idx, text in enumerate(self.dialogBytes):
+            lenText[idx] = len(text)
+            ptrText[idx+1] = ptrText[idx] + lenText[idx]
+        sumBytes = sum(lenText)
+
+        if preSize < sumBytes:
+            logging.warning(f"check the dialogs length, size overflowed; privious({preSize}) < current({sumBytes})")
+
+        byte_stream = io.BytesIO(self.buffer)
+        for idx in range(numTexts):
+            byte_stream.write(bytes2(ptrText[idx]//2))
+
+        for text in self.dialogBytes:
+            byte_stream.write(text)
+
+        return byte_stream.getbuffer()
+    
+class ScriptSection(SectionBase):
+    def __init__(self, buffer: Union[bytes, None] = None) -> None:
+        self.buffer = bytearray(buffer) if buffer is not None else None
+
+        self.scriptOpcodes      = SectionBase()
+        self.dialogText         = DialogText()
+        self.unknownSection1    = SectionBase()
+        self.unknownSection2    = SectionBase()
+
+        if buffer is not None:
+            self.unpackData(buffer)
+
+    def unpackData(self, buffer: bytes):
+        if buffer is not None:
+            self.buffer = bytearray(buffer)
+        else:
+            return
+        
+        byte_stream = io.BytesIO(self.buffer)
+        header = readHeader(byte_stream, 8, 2)
+        
+        poses = [16, header[1], header[2], header[3]]
+        sizes = [poses[1]-poses[0], poses[2]-poses[1], poses[3]-poses[2], header[0]-poses[3]]
+
+        sections = [self.scriptOpcodes, self.dialogText, self.unknownSection1, self.unknownSection2]
+        for idx in range(4):
+            if sizes[idx] == 0: continue
+            byte_stream.seek(poses[idx])
+            sections[idx].unpackData(byte_stream.read(sizes[idx]))
+
+    def packData(self):
+        if self.buffer is None:
+            return None
+
+        byte_stream = io.BytesIO(self.buffer)
+        header = readHeader(byte_stream, 8, 2)
+        poses = [16]
+        sizes = []
+        sections = [self.scriptOpcodes, self.dialogText, self.unknownSection1, self.unknownSection2]
+
+        for idx in range(4):
+            sizes.append(len(sections[idx]))
+            if idx > 0:
+                poses.append(poses[idx-1] + sizes[idx-1])
+        sumSizes = sum(sizes) + 16
+
+        if sumSizes > header[0]:
+            logging.warning(f"check the ScriptSection size, size overflowed({sumSizes} > {header[0]})")
+
+        byte_stream.seek(0)
+        byte_stream.write(bytes2(sumSizes))
+        byte_stream.write(bytes2(poses[1]))
+        byte_stream.write(bytes2(poses[2]))
+        byte_stream.write(bytes2(poses[3]))
+        byte_stream.write(bytes2(header[4]))
+        byte_stream.write(bytes2(header[5]))
+        byte_stream.write(bytes2(header[6]))
+        byte_stream.write(bytes2(header[7]))
+
+        for idx in range(4):
+            data = sections[idx].packData()
+            if data is not None:
+                byte_stream.seek(poses[idx])
+                byte_stream.write(data)
+
+        return byte_stream.getbuffer()
+
+class MPDstruct():
+    def __init__(self, input_path:str = '') -> None:
+        self.buffer = None
+        self.roomSection        = SectionBase()
+        self.clearedSection     = SectionBase()
+        self.scriptSection      = ScriptSection()
+        self.doorSection        = SectionBase()
+        self.enemySection       = SectionBase()
+        self.treasureSection    = SectionBase()
+
+        if input_path:
+            self.unpackData(input_path)
+
+    def unpackData(self, input_path:str):
+        with open(input_path, 'rb') as file:
+            self.buffer = bytearray(file.read())
+            byte_stream = io.BytesIO(self.buffer)
+
+            header = readHeader(byte_stream, 12, 4)
+            poses = [header[0], header[2], header[4], header[6], header[8], header[10]]
+            sizes = [header[1], header[3], header[5], header[7], header[9], header[11]]
+
+            sections = [self.roomSection, self.clearedSection, self.scriptSection, self.doorSection, self.enemySection, self.treasureSection]
+
+            for idx in range(6):
+                if sizes[idx] == 0: continue
+                byte_stream.seek(poses[idx])
+                sections[idx].unpackData(byte_stream.read(sizes[idx]))
+
+    def packData(self, output_path:str):
+        if self.buffer is None:
+            return
+        
+        fileSize = len(self.buffer)
+        byte_stream = io.BytesIO(self.buffer)
+
+        header = readHeader(byte_stream, 12, 4)
+        poses = [header[0]]
+        sizes = []
+        
+        sections = [self.roomSection, self.clearedSection, self.scriptSection, self.doorSection, self.enemySection, self.treasureSection]
+
+        for idx in range(6):
+            sizes.append(len(sections[idx]))
+            if idx > 0:
+                poses.append(poses[idx-1] + sizes[idx-1])
+        sumSizes = sum(sizes)
+
+        scriptSection = self.scriptSection.packData()
+        maxScriptSectionSize = header[6] - header[4]
+        writeSize = len(scriptSection) if scriptSection is not None else 0
+        if writeSize > maxScriptSectionSize:
+            logging.warning(f"check the section size, size overflowed({writeSize} > {maxScriptSectionSize})")
+
+        if sumSizes > fileSize:
+            prev = math.ceil(fileSize / 2048) * 2048
+            curr = math.ceil(sumSizes / 2048) * 2048
+            if curr > prev:
+                logging.critical(f"check the file size, LBA overflowed({sumSizes} > {fileSize})")
+            else:
+                logging.warning(f"check the file size, size overflowed({sumSizes} > {fileSize})")
+
+        with open(output_path, 'wb') as file:
+            for idx in range(6):
+                file.write(bytes4(poses[idx]))
+                file.write(bytes4(sizes[idx]))
+
+            for idx in range(6):
+                data = scriptSection if idx == 2 else sections[idx].packData()
+                if data is not None:
+                    file.seek(poses[idx])
+                    file.write(data)
+
+mpd = MPDstruct()
+mpd.unpackData("D:/Projects/vagrant_story_korean/font/test/jpn/MAP001.MPD")
+mpd.packData("D:/Projects/vagrant_story_korean/MAP001.MPD")
+
