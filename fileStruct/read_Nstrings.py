8bc12f73fbd68eed3d6ad78e18325c0c8882cd5a
moonhyun.lee <moonhyun.lee@lge.com>
2024-10-18 17:50:09 +0900

    update file unpack

--- fileStruct/read_Nstrings.py
+++ fileStruct/read_Nstrings.py
@@ -1,16 +1,37 @@
-from typing import Union, List
+from typing import Union, List, Tuple
 import os
 import logging
 from pathlib import Path
 from font.dialog import convert_by_TBL
 from fileStruct.readStrFile import ReadStrings
+from fileStruct.readWordFile import ReadWords
 
+class WordPos:
+    def __init__(self, ptr: int, length: int, num: int, stride = -1) -> None:
+        self.Ptr = ptr
+        self.Length = length
+        self.Num = num
+        self.Stride = stride if 0 < stride else length
 
-def createNstringsClass(FileName: str, NstringPtrs: List[int]):
+class Class_Nstrings:
+    def __init__(self, input_path: str = '') -> None:
+        self.strings: List[ReadStrings] = []
+        self.words: List[ReadWords] = []
+    def cvtStr2Byte(self, table: convert_by_TBL) -> None:
+        pass
+    def cvtByte2Str(self, table: convert_by_TBL) -> None:
+        pass
+    def unpackData(self, input_path: str) -> None:
+        pass
+    def packData(self, output_path: str) -> None:
+        pass
+
+def createNstringsNwordsClass(FileName: str, NstringPtrs: List[int], Nwors: List[WordPos] = []):
     class Class_Nstrings(): 
         def __init__(self, input_path: str = '') -> None:
             self.buffer = bytes()
             self.strings: List[ReadStrings] = []
+            self.words: List[ReadWords] = []
 
             if os.path.isfile(input_path):
                 self.unpackData(input_path)
@@ -26,10 +47,16 @@ def createNstringsClass(FileName: str, NstringPtrs: List[int]):
         def cvtStr2Byte(self, table: convert_by_TBL):
             for strings in self.strings:
                 strings.cvtStr2Byte(table)
+                
+            for words in self.words:
+                words.cvtStr2Byte(table)
 
         def cvtByte2Str(self, table: convert_by_TBL):
             for strings in self.strings:
                 strings.cvtByte2Str(table)
+            
+            for words in self.words:
+                words.cvtByte2Str(table)
 
         def unpackData(self, input_path: str):
             with open(input_path, 'rb') as file:
@@ -38,9 +65,22 @@ def createNstringsClass(FileName: str, NstringPtrs: List[int]):
                 self.strings.clear()
                 for ptr in NstringPtrs:
                     self.strings.append(ReadStrings(self.buffer[ptr:]))
+                
+                self.words.clear()
+                for ptr in Nwors:
+                    self.words.append(ReadWords(ptr.Length, ptr.Num, self.buffer[ptr.Ptr : ptr.Ptr + ptr.Num*ptr.Stride], ptr.Stride))
 
         def packData(self, output_path: str):
-            with open(output_path, 'wb') as file:
+            outPath = ''
+            if os.path.isfile(output_path):
+                outPath = output_path
+            elif os.path.isdir(output_path):
+                filepath = Path(output_path) / Path(FileName)
+                outPath = str(filepath)
+            else:
+                logging.warning(f'{output_path} is not valid path.')
+                
+            with open(outPath, 'wb') as file:
                 file.write(self.buffer)
                 
                 for idx in range(len(NstringPtrs)):
@@ -48,48 +88,195 @@ def createNstringsClass(FileName: str, NstringPtrs: List[int]):
                     if byteData is not None:
                         file.seek(NstringPtrs[idx])
                         file.write(byteData)
+                
+                for idx in range(len(Nwors)):
+                    byteData = self.words[idx].packData()
+                    if byteData is not None:
+                        file.seek(Nwors[idx].Ptr)
+                        file.write(byteData)
 
     return Class_Nstrings
 
+MENU0 = createNstringsNwordsClass('MENU/MENU0.PRG', [0x2258])
+MENU1 = createNstringsNwordsClass('MENU/MENU1.PRG', [0xC78])
+MENU2 = createNstringsNwordsClass('MENU/MENU2.PRG', [0x1e90])
+MENU3 = createNstringsNwordsClass('MENU/MENU3.PRG', [0x6bb8])
 
+MENU4_en = createNstringsNwordsClass('MENU/MENU4.PRG', [0x4C48])
+MENU4_jp = createNstringsNwordsClass('MENU/MENU4.PRG', [0x4c44])
 
-jpStringPtrs = [0x5f9c, 0x5fc8, 0x611c, 0x6320, 0x671c, 0x6850, 0x6ca8, 0x6d00, 0x6e6c]
-MENU9_PRG_jp = createNstringsClass('MENU/MENU9.PRG', jpStringPtrs)
+MENU5_en = createNstringsNwordsClass('MENU/MENU5.PRG', [0x5bfc])
+MENU5_jp = createNstringsNwordsClass('MENU/MENU5.PRG', [0x5c14])
 
-enStringPtrs = [0x5d20, 0x5d64, 0x5ee4, 0x61bc, 0x67dc, 0x6a00, 0x6d98, 0x6e0c, 0x6fdc]
-MENU9_PRG_en = createNstringsClass('MENU/MENU9.PRG', enStringPtrs)
+MENU7_en = createNstringsNwordsClass('MENU/MENU7.PRG', [0x81b0])
+MENU7_jp = createNstringsNwordsClass('MENU/MENU7.PRG', [0x7c54])
 
+MENU8_en = createNstringsNwordsClass('MENU/MENU8.PRG', [0x2d58])
+MENU8_jp = createNstringsNwordsClass('MENU/MENU8.PRG', [0x429c])
 
-def read_MENU9():
-    MENU9_jp = MENU9_PRG_jp(PATH_JPN_VARGRANTSTORY)
-    MENU9_jp.cvtByte2Str(jpnTBL)
+MENU9_jp_Ptrs = [0x5f9c, 0x5fc8, 0x611c, 0x6320, 0x671c, 0x6850, 0x6ca8, 0x6d00, 0x6e6c]
+MENU9_jp = createNstringsNwordsClass('MENU/MENU9.PRG', MENU9_jp_Ptrs)
+
+MENU9_en_Ptrs = [0x5d20, 0x5d64, 0x5ee4, 0x61bc, 0x67dc, 0x6a00, 0x6d98, 0x6e0c, 0x6fdc]
+MENU9_en = createNstringsNwordsClass('MENU/MENU9.PRG', MENU9_en_Ptrs)
+
+MENUB = createNstringsNwordsClass('MENU/MENUB.PRG', [0x7a80])
+
+MENUD_en = createNstringsNwordsClass('MENU/MENUD.PRG', [0x6d2c])
+MENUD_jp = createNstringsNwordsClass('MENU/MENUD.PRG', [0x6d30])
+
+MENUE_en = createNstringsNwordsClass('MENU/MENUE.PRG', [0x2654])
+MENUE_jp = createNstringsNwordsClass('MENU/MENUE.PRG', [0x2644])
+
+MENU12 = createNstringsNwordsClass('MENU/MENU12.BIN', [0x0])
+MCMAN = createNstringsNwordsClass('MENU/MCMAN.BIN', [0x0])
+ITEMHELP = createNstringsNwordsClass('MENU/ITEMHELP.BIN', [0x0])
+
+BATTLE_en = createNstringsNwordsClass('BATTLE/BATTLE.PRG', [0x82068, 0x831DC, 0x835DC], [WordPos(0x83758, 0x18, 21)])
+BATTLE_jp = createNstringsNwordsClass('BATTLE/BATTLE.PRG', [0x82050, 0x83080, 0x8341C], [WordPos(0x83520, 0x18, 21)])
+
+MON = createNstringsNwordsClass('SMALL/MON.BIN', [0x19C8], [WordPos(0x12, 0x1A, 150, 0x2C)])
+
+TITLE_en = createNstringsNwordsClass('TITLE/TITLE.PRG', [], [WordPos(0xc42C, 0x18, 8, 0x20)])
+TITLE_jp = createNstringsNwordsClass('TITLE/TITLE.PRG', [], [WordPos(0xA58C, 0x18, 8, 0x20)])
+
+ITEMNAME = createNstringsNwordsClass('MENU/ITEMNAME.BIN', [], [WordPos(0x0, 0x18, 512)])
+
+SL_Main_en = createNstringsNwordsClass('SLUS_010.40', [], [WordPos(0x3C1F8, 0x18, 256, 0x34)])
+SL_Main_jp = createNstringsNwordsClass('SLPS_023.77',[], [WordPos(0x3C1F8, 0x18, 256, 0x34)])
+
+FileLoadFuncNames = ['MENU0', 'MENU1', 'MENU2', 'MENU3', 'MENU4', 'MENU5', 
+                     'MENU7', 'MENU8', 'MENU9', 'MENUB', 'MENUD', 'MENUE', 
+                     'MENU12', 'MCMAN', 'ITEMHELP', 'BATTLE', 'MON', 'TITLE',
+                     'ITEMNAME', 'SL_Main']
+
+FileLoad1Strs = ['MENU0', 'MENU1', 'MENU2', 'MENU3', 'MENU4', 'MENU5', 
+                 'MENU7', 'MENU8', 'MENUB', 'MENUD', 'MENUE', 'MENU12', 
+                 'MCMAN', 'ITEMHELP', 'TITLE', 'ITEMNAME', 'SL_Main']
+
+def getNNClass(className: str) -> Tuple[Class_Nstrings, Class_Nstrings]:
+    Class_en = None
+    Class_jp = None
     
-    MENU9_en = MENU9_PRG_en(PATH_USA_VARGRANTSTORY)
-    MENU9_en.cvtByte2Str(usaTBL)
+    if className in globals():
+        Class_en = globals()[className]
+        Class_jp = globals()[className]
+        
+    if Class_en is None:
+        if f'{className}_en' in globals():
+            Class_en = globals()[f'{className}_en']
+        if Class_en is None:
+            logging.warning(f'wrong function name {className}')
+        
+    if Class_jp is None:
+        if f'{className}_jp' in globals():
+            Class_jp = globals()[f'{className}_jp']
+        if Class_jp is None:    
+            logging.warning(f'wrong function name {className}')
     
-    jpntexts = {}
-    for idx0, strings in enumerate(MENU9_jp.strings):
-        lv1 = {}
-        for idx1, text in enumerate(strings._str):
-            lv1[f"{idx1:03}"] = text
-        jpntexts[f"{idx0:02}"] = lv1
+    return Class_jp, Class_en
+
+def makeNNstrings(fileSrc: Class_Nstrings, fileComment: Class_Nstrings):
+    str_group = {}
     
-
-    usatexts = {}
-    for idx0, strings in enumerate(MENU9_en.strings):
-        lv1 = {}
-        for idx1, text in enumerate(strings._str):
-            lv1[f"{idx1:03}"] = text
-        usatexts[f"{idx0:02}"] = lv1
-
-    jpntexts = {}
-    for k0, v1 in jpntexts.items():
+    len_strings = len(fileSrc.strings)
+    for idx0 in range(len_strings):
+        len_str = fileSrc.strings[idx0].itemNums
+        if 0 == len_str:
+            continue
+        
         texts = {}
-        for k1, v2 in v1.items():
-            single = { 'string': v2, '@@localazy:comment:string': usatexts[k0][k1] }
-            texts[k1] = single
-        jpntexts[k0] = texts
+        for idx1 in range(len_str):
+            src = fileSrc.strings[idx0]._str[idx1]
+            if not src: 
+                continue
+            
+            comment = fileComment.strings[idx0]._str[idx1]
+            singleRow = {}
+            singleRow['string'] = src
+            if comment:
+                singleRow['@@localazy:comment:string'] = comment
+            texts[f'{idx1:03}'] = singleRow
     
-    with open(f'work/strings/MENU_MENU9_ja.json', 'w', encoding='utf-8') as f:
-        json.dump(jpntexts, f, indent=2, ensure_ascii=False)
+        str_group[f'str_{idx0}'] = texts
+    
+    len_words = len(fileSrc.words)
+    for idx0 in range(len_words):
+        len_str = fileSrc.words[idx0].wordNums
+        if 0 == len_str:
+            continue
+        
+        texts = {}
+        for idx1 in range(len_str):
+            src = fileSrc.words[idx0]._str[idx1]
+            if not src: 
+                continue
+            
+            comment = fileComment.words[idx0]._str[idx1]
+            singleRow = {}
+            singleRow['string'] = src
+            if comment:
+                singleRow['@@localazy:comment:string'] = comment
+            texts[f'{idx1:03}'] = singleRow
+    
+        str_group[f'word_{idx0}'] = texts
+        
+    return str_group
+
+
+def testNNclass():
+    for name in rN.FileLoadFuncNames:
+        ClassJp, ClassEn = rN.getNNClass(name)        
+        loadJp = ClassJp(PATH_JPN_VARGRANTSTORY)
+        loadEn = ClassEn(PATH_USA_VARGRANTSTORY)
+        
+        loadJp.cvtByte2Str(jpnTBL)
+        loadEn.cvtByte2Str(usaTBL)
+        
+        ####
+        texts = rN.makeNNstrings(loadJp, loadEn)
+        
+        textNstr = {}
+        if name in rN.FileLoad1Strs:
+            if texts.get('str_0') is not None:
+                textNstr[name] = texts['str_0']
+            if texts.get('word_0') is not None:
+                textNstr[name] = texts['word_0']
+            
+        if name == 'MENU9':
+            for idx in range(9):
+                textNstr[f"{name}_{idx+1}"] = texts[f'str_{idx}']
+        
+        if name == 'BATTLE':
+            textNstr[f"BATTLE_1"] = texts[f'str_0']
+            textNstr[f"BATTLE_2"] = texts[f'word_0']
+            textNstr[f"BATTLE_3"] = texts[f'str_1']
+            textNstr[f"BATTLE_4"] = texts[f'str_2']
+                
+        
+        if name == 'MON':
+            subText = {}
+
+            for idx in range(150):
+                monName = texts['word_0'].get(f'{idx:03}')
+                monDesc = texts['str_0'].get(f'{idx:03}')
+                if monDesc is not None:
+                    subText[f"{idx:03}"] = {'name':monName['string'], 
+                                            "@@localazy:comment:name":monName['@@localazy:comment:string'], 
+                                            'description':monDesc['string'], 
+                                            "@@localazy:comment:description":monDesc['@@localazy:comment:string']}
+                else:
+                    subText[f"{idx:03}"] = {'name':monName['string'], 
+                                            "@@localazy:comment:name":monName['@@localazy:comment:string']}
+            textNstr['MON'] = subText
+        with open(f'work/test/{name}_jp.json', 'w', encoding='utf-8') as f:
+            json.dump(textNstr, f, indent=2, ensure_ascii=False)
+        ####
+        
+        loadJp.cvtStr2Byte(jpnTBL)
+        loadEn.cvtStr2Byte(usaTBL)
+        
+        loadJp.packData('work/test/PACKjp')
+        loadEn.packData('work/test/PACKen')
+
   
